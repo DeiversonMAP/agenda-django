c366dd1c8bb3b20206054cf0ea8860b645ca5116
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2023-02-03 22:49:05 -0300

    Criaçãod e super usuario e exibição de valores nas Views

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id','nome', 'sobrenome','telefone','email','categoria', 'data_criacao')
+    list_display_links = ('nome', 'sobrenome')
+    list_filter = ('categoria',)
+    list_per_page = 10
+    search_fields = ('nome', 'sobrenome', 'categoria')
+
+admin.site.register(Categoria) 
+admin.site.register(Contato,ContatoAdmin) 
--- contatos/models.py
+++ contatos/models.py
@@ -3,8 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 
+# Os ids das tabelas são gerados automaticamente
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
+    
+    #metodo criado para alterar a representaçãp do objeto dentro do admin
+    def __str__(self) -> str: 
+        return self.nome
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -14,4 +19,7 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    
+
+    def __str__(self) -> str: 
+        return f'{self.nome.title()} {self.sobrenome.title()} - {self.categoria.nome}'
+        # return self.nome + ' ' + self.sobrenome
