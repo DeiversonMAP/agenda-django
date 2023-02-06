7a8ef1d5aff2e91d672f0a70f059a19772c317ae
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2023-02-05 22:43:09 -0300

    Arrumando exibição de um único contatos por vez

--- contatos/views.py
+++ contatos/views.py
@@ -5,7 +5,12 @@ from .models import Contato
 
 # Create your views here.
 def index(request):
-    contato=Contato.objects.all()
+    contatos=Contato.objects.all()
     return render(request,'contatos/index.html',{
-        'contatos': contato
+        'contatos': contatos
+    })
+def ver_contato(request,contato_id):
+    contato=Contato.objects.get(id=contato_id)
+    return render(request,'contatos/ver_contato.html',{
+        'contato': contato
     })
