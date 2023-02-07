c0ee1c64a1f4bee46bf8742e3f254b05a01be331
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2023-02-07 19:42:55 -0300

    Aprendendo a ordenar e filtrar os dados de exibição

--- contatos/views.py
+++ contatos/views.py
@@ -7,7 +7,10 @@ from .models import Contato
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    # contatos = Contato.objects.all()
+    # contatos = Contato.objects.order_by('id') #Ordem crescente
+    # contatos = Contato.objects.order_by('-id') #Ordem decrescente
+    contatos = Contato.objects.order_by('id').filter(mostrar=True) # Filtranto contatos que tenham o campo mostrar igual a True
     
     # https://docs.djangoproject.com/en/2.2/topics/pagination/
     paginator = Paginator(contatos,20)
@@ -21,6 +24,9 @@ def index(request):
 def ver_contato(request,contato_id):
     # contato=Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato,id=contato_id)
+    
+    if not contato.mostrar:
+        raise Http404()
 
     return render(request,'contatos/ver_contato.html',{
         'contato': contato
